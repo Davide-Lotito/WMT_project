9b11c8051a2a8cc8ddf1eb3497e826c32e4b1f0f
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-11 22:31:36 +0100

    main page stuff
    delete kitchen page
    change font

--- script/index.js
+++ script/index.js
@@ -1,11 +1,12 @@
+let slides = document.getElementsByClassName("single-slide");
+let dots = document.getElementsByClassName("dot");
+
 function showAuto() {
     let slideIndex = 0;
     showSlides();
 
     function showSlides() {
         let i;
-        let slides = document.getElementsByClassName("single-slide");
-        let dots = document.getElementsByClassName("dot");
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
@@ -16,7 +17,17 @@ function showAuto() {
         if (slideIndex > slides.length) { slideIndex = 1 }
         dots[slideIndex - 1].className += " active";
         slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 5000); // Change automatic images every 5 seconds
+        setTimeout(showSlides, 3000); // Change automatic images every 5 seconds
+    }
+}
+
+function currentSlide(index){
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        if(i == index-1){
+            slides[index-1].style.display = "block";
+            dots[index - 1].className += " active";
+        }
     }
 }
 
