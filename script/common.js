bbe2b2ee2816929a18a349c5c02ff1aa08f2dc7f
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-13 00:29:13 +0100

    add a progress bar on top

--- script/common.js
+++ script/common.js
@@ -1,9 +1,6 @@
-
-let onTop = document.getElementById("back-on-top");
-
-window.onscroll = function () { scrollFunction() };
-
+/*check how far you scroll the page*/
 function scrollFunction() {
+    let onTop = document.getElementById("back-on-top");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         onTop.style.display = "block";
     } else {
@@ -11,7 +8,22 @@ function scrollFunction() {
     }
 }
 
+/*Back on top*/
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+/*Show the progress bar*/
+function progressBar() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+/*groups function on the same event*/
+window.onscroll = function () {
+    scrollFunction();
+    progressBar();
 }
