4edb687e819429559e501abed9d349304eda7b24
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-13 02:52:58 +0100

    add some check JS function
    for the "reservation" page

--- script/book.js
+++ script/book.js
@@ -1,4 +1,14 @@
-function checkDate() {
+function empty(inputtx) 
+{
+  if (inputtx.value.length == 0)
+   { 
+    //   alert("empty field");  	
+      return false; 
+   }  	
+   return true; 
+} 
+
+function checkDateTime() {
     let dateO = document.getElementById("date");
     let date = dateO.value;
     let currentDate = new Date();
@@ -10,25 +20,83 @@ function checkDate() {
     let month = date.slice(5, 7);
     let year = date.slice(0, 4);
 
-    if (day == "" || year == "" || day == "") {
-        return 1;
+    // if (day == "" || year == "" || day == "") {
+    //     return true;
+    // }
+    if(empty(date)){
+        return false;
     }
 
     if (year - currentYear >= 0) {
+        // console.log("Year is ok");
         if (month >= currentMonth) {
+            // console.log("Month is ok");
             if (month == currentMonth) {
                 if (day >= currentDay) {
+                    // console.log("It is ok");
+                    checkTime();
                 } else {
                     alert("Invalid Date --> invalid day");
                     dateO.value = "";
+                    return false;
                 }
             }
+            // console.log("It is ok");
+            checkTime();
         } else {
             alert("Invalid Date --> invalid month");
             dateO.value = "";
+            return false;
         }
     } else {
         alert("Invalid Date --> invalid year");
         dateO.value = "";
+        return false;
     }
+}
+
+function checkTime() {
+    let timeO = document.getElementById("time");
+    let time = timeO.value;
+    let currentDate = new Date();
+    let currentHours = currentDate.getHours();
+    let currentMinutes = currentDate.getMinutes();
+    let currentTime = `${currentHours}:${currentMinutes}`;
+    
+    let hours = time.slice(0,2);
+    let minutes = time.slice(2);
+
+    // if (time == "") {
+    //     return null;
+    // }
+    if(empty(time)){
+        return false;
+    }
+
+    if (hours - currentHours >= 0){
+        // console.log("Time is ok");
+        if(hours == currentHours){
+            if(minutes > currentMinutes){
+                // console.log("Time is ok");
+                return true;
+            } else {
+                alert("Invalid Time --> hours");
+                return false;
+            }
+        }
+    } else {
+        alert("Invalid Time --> hours");
+        return false;
+    }
+}
+
+submitButton = document.getElementById("submit-button")
+submitButton.onsubmit = function(){
+    if(checkDateTime()){
+        alert("valid")
+        return 1;
+    }
+    alert("invalid");
+    return 0;
+    // console.log("ciao");
 }
