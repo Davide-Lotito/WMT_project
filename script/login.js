99306a23bf7bf8aca552188ade0f0442a88fc5de
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-15 15:29:14 +0100

    add another control on the password

--- script/login.js
+++ script/login.js
@@ -4,21 +4,42 @@
  * @param {*} input
  * @returns true if is ok
  */
- function checks(input) {
-    return ((input.length < MAXCHARACTERS)&&(input.length >= MINCHARACTERS )) ? true : false;
+function size(input) {
+    return ((input.length < MAXCHARACTERS) && (input.length >= MINCHARACTERS)) ? true : false;
+}
+
+/**
+ * Check if the the password has at least UPPERCASE letter in upperCase
+ * @param {*} input 
+ * @returns true if is ok
+ */
+function upperCase(input) {
+    let upper = 0;
+    for (let i = 0; i < input.length; i++) {
+        if (input.charAt(i) == input.charAt(i).toUpperCase()) {
+            upper++;
+        }
+    }
+    return (upper >= UPPERCASE) ? true : false;
 }
 
 //----------------------- GLOBAL VARIABLES ----------------------- //
 const MINCHARACTERS = 5;
 const MAXCHARACTERS = 10;
+const UPPERCASE = 1;
 
 // ---------------------- EVENTS HANDLING ------------------------ //
 const passwordO = document.getElementById("pswd");
 
-passwordO.addEventListener("focusout", ()=>{
+passwordO.addEventListener("focusout", () => {
     let pswd = passwordO.value;
     // console.log(pswd.length);
-    if(!checks(pswd)){
+    if (!size(pswd)) {
         alert(`Wrong password! At least ${MINCHARACTERS} and maximum ${MAXCHARACTERS}`);
+        return 0;
+    }
+    if (!upperCase(pswd)){
+        alert(`Wrong password! At least ${UPPERCASE} characters in upper case`);
+        return 0
     }
 })
