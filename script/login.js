5118d6978fb06194afadd90c7a92a07156f4564d
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-15 15:47:11 +0100

    working on check pswd

--- script/login.js
+++ script/login.js
@@ -23,10 +23,25 @@ function upperCase(input) {
     return (upper >= UPPERCASE) ? true : false;
 }
 
+// ------------------------------------------------------ TRANSFORM array in a string e poi la sua size !!
+// function numbers(input) {
+//     var regex = /\d+/;
+//     let n;
+//     var m;
+//     for (let i = 0; i < input.length; i++) {
+//         if (input.match(regex)) {
+//             m = input.match(regex).join(" -").length;
+//             console.log(input.match(regex).join("-"));
+//         }
+//     }
+//     return (m >= NUMBERS) ? true : false;
+// }
+
 //----------------------- GLOBAL VARIABLES ----------------------- //
 const MINCHARACTERS = 5;
 const MAXCHARACTERS = 10;
 const UPPERCASE = 1;
+const NUMBERS = 1;
 
 // ---------------------- EVENTS HANDLING ------------------------ //
 const passwordO = document.getElementById("pswd");
@@ -38,8 +53,13 @@ passwordO.addEventListener("focusout", () => {
         alert(`Wrong password! At least ${MINCHARACTERS} and maximum ${MAXCHARACTERS}`);
         return 0;
     }
-    if (!upperCase(pswd)){
+    if (!upperCase(pswd)) {
         alert(`Wrong password! At least ${UPPERCASE} characters in upper case`);
         return 0
     }
+    if (!numbers(pswd)) {
+        alert(`Wrong password! At least ${NUMBERS} characters have to be numbers`);
+        return 0
+    }
+    // console.log(numbers(pswd))
 })
