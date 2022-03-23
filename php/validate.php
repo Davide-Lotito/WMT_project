de4d8d5424c1d366ba5c86bc43b6d2d9257e3230
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-23 19:19:48 +0100

    correct login bug

--- php/validate.php
+++ php/validate.php
@@ -36,27 +36,26 @@
         foreach ($results as $user) {
             if (($user['adminname'] == $adminname) && ($user['password'] == $password)) {
                 header("Location: ../pages/dashboard.php");
-            } else {
-                echo'
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Wrong Login</title>
-                    <link rel="stylesheet" href="../style/common.css">
-                    <link rel="icon" type="image/png" href="../images/chef_white_icon.png">
-                </head>
-                    <body>
-                        <div class="center">
-                            <h1 class="title-font">Username/password incorrect.</h3><br><br><h2>Click here to <a href="../pages/login.php">Login</a></h2>
-                        </div>
-                    </body>
-                </html>
-                ';
-                die();
             }
         }
+        echo'
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Wrong Login</title>
+            <link rel="stylesheet" href="../style/common.css">
+            <link rel="icon" type="image/png" href="../images/chef_white_icon.png">
+        </head>
+            <body>
+                <div class="center">
+                    <h1 class="title-font">Username/password incorrect.</h3><br><br><h2>Click here to <a href="../pages/login.php">Login</a></h2>
+                </div>
+            </body>
+        </html>
+        ';
+        die();
     }
 ?>
