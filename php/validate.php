65475372ca8075e29db4d899df89659f93285e68
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-23 22:18:18 +0100

    refactor

--- php/validate.php
+++ php/validate.php
@@ -10,6 +10,13 @@
         return $data;
     }
 
+    function wrongLogin($msg) {
+        echo '<script type="text/javascript">';
+        echo 'alert("'.$msg.'");';
+        echo 'window.location = "../pages/login.php"';
+        echo '</script>';
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $adminname = test_input($_POST["name"]);
         $password = test_input($_POST["pswd"]);
@@ -21,24 +28,7 @@
                 header("Location: ../pages/dashboard.php");
             }
         }
-        echo'
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Wrong Login</title>
-            <link rel="stylesheet" href="../style/common.css">
-            <link rel="icon" type="image/png" href="../images/chef_white_icon.png">
-        </head>
-            <body>
-                <div class="center">
-                    <h1 class="title-font">Username/password incorrect.</h3><br><br><h2>Click here to <a href="../pages/login.php">Login</a></h2>
-                </div>
-            </body>
-        </html>
-        ';
+        wrongLogin("Wrong username or password!");
         die();
     }
 ?>
