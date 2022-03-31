90b41f83409b7de8327bc283ade22f795cbf3159
Davide-Lotito <80196658+Davide-Lotito@users.noreply.github.com>
2022-03-31 17:13:03 +0200

    remove old reservation by time

--- php/deleteOld.php
+++ php/deleteOld.php
@@ -2,7 +2,12 @@
 <?php
     function deleteOldReservation($conn){
         $d = date("Y-m-d", strtotime('today'));
-        $sql = "DELETE FROM `reservations` WHERE `date` < '$d' ";
+        if(date('H:i:s', time()+7200) > '16:00'){
+            $turn = 'af';
+        } else {
+            $turn = 'mo';
+        }
+        $sql = "DELETE FROM `reservations` WHERE `date` = '$d' and `turn` != '$turn' ";
         $result = $conn->query($sql);
         return $result;
     }
